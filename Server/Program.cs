a844f1c1a47405c14f31a02fe13343d912fd8161
Gorrian <gorriansan@hotmail.com>
2022-01-25 01:45:55 +0100

    TCP_server starting point
    Started working on the server side by adding code from a previous test I made with TCP connections

---
+++ Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener Listener = new TcpListener(IPAddress.Any, Int32.Parse(args[0]));
+            Listener.Start();
+            Console.WriteLine("Servidor funcionando en puerto ("+args[0]+")");
+            Console.WriteLine("Esperando conexion...");
+
+            Socket Puerta = Listener.AcceptSocket();
+            
+            Console.WriteLine("Conexion aceptada de "+ Puerta.RemoteEndPoint);
+            Console.WriteLine("Esperando mensaje");
+            byte[] Recive=new byte[100];
+            int longitud=Puerta.Receive(Recive);
+        }
+    }
+}
